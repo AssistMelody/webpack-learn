326a45aac3576408462fb80963efaa853e2af11d
AssistMelody <s41018219961010@gmail.com>
2019-11-30 16:07:19 +0800

    learn-图片资源

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log('test')
+import avatar from './asstes/test.png'
+var img = new Image()
+img.src = avatar;
+document.body.append(img);
+console.log('index')
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,23 @@
 var path = require('path');
 module.exports = {
+    mode:'development',
     entry:'./src/index.js',
+    module:{
+        rules:[
+            {
+                test:/\.(png|jpg|gif)$/,
+                use:{
+                    loader:'url-loader',
+                    options:{
+                        // placeholder[] 占位符
+                        name:'[name]-[hash].[ext]',
+                        outputPath: 'images/',
+                        limit:204800 // url-loader
+                    }
+                }
+            }
+        ]
+    },
     output:{
         filename:'bundle.js',
         path:path.resolve(__dirname,'dist')
